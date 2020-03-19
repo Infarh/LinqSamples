d9b5da652aed9e5f7ad4b38405eeec1c752c4a1a
Infarh <shmachilin@yandex.ru>
2020-03-19 20:30:48 +0300

    Добавьте файлы проекта.

---
+++ LinqSamples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://megagenerator.ru/namefio/
+
+
+
+            Console.WriteLine("Программа завершена!");
+            Console.ReadLine();
+        }
+    }
+}
